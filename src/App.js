afb5005bc12c4e027ad1027ba96b05b5ca63615a
DayaToledo <dayanabritotoledo_@hotmail.com>
2021-05-06 23:16:45 -0300

    init application

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Desafio prático BGC
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
