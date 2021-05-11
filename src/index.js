02293de0d14c9bb452518d08f46496740b242b0c
DayaToledo <dayanabritotoledo_@hotmail.com>
2021-05-11 17:07:26 -0300

    feat: configure AWS Amplify

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Amplify } from 'aws-amplify';
+import config from './services/config'
+
+Amplify.configure({
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+  },
+  API: {
+    endpoints: [
+      {
+        name: "emails",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      },
+    ]
+  }
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
---
+++ src/services/config.js
@@ -0,0 +1,12 @@
+const config = {
+    s3: {
+        REGION: "sa-east-1",
+        BUCKET: "cupidoonline-uploads",
+    },
+    apiGateway: {
+        REGION: "sa-east-1",
+        URL: "https://yyln21ys9d.execute-api.sa-east-1.amazonaws.com/dev",
+    },
+};
+
+export default config;
