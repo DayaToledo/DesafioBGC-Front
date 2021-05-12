a00b15715df504e8f65fe8b84cd157f4c6863c6b
DayaToledo <dayanabritotoledo_@hotmail.com>
2021-05-12 01:51:25 -0300

    feat: add toastify for notification

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,25 +1,37 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { API } from 'aws-amplify';
+import { ToastContainer, toast } from 'react-toastify';
 
 import { Container } from './styles';
 
+import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.min.css';
+
 export default function Form() {
     const history = useHistory();
+
     const [values, setValues] = useState({
         name: '',
         email: '',
         text: '',
     });
 
+    const notify = (message, type) => {
+        if (type == "error") toast.error(message);
+        else if (type == "info") toast.info(message);
+        else if (type == "warning") toast.warn(message);
+        else if (type == "success") toast.success(message);
+        else toast(message);
+    };
+
     async function sendMail(body) {
         try {
             await API.post("emails", "/send", {body});
-            console.log(body);
-            console.log("E-mail enviado com sucesso!")
+            notify("E-mail enviado com sucesso!", "success");
         } catch (e) {
-            console.log('Falha ao enviar o e-mail!');
-            console.log(e);
+            console.error(e);
+            notify("Falha ao enviar o e-mail!", "error");
         }
     }
 
@@ -30,7 +42,7 @@ export default function Form() {
     async function handleSubmit(event) {
         event.preventDefault();
 
-        if (!name || !email || !text) console.log("Preencha todos os campos!");
+        if (!name || !email || !text) notify("Preencha todos os campos!", "info");
         else {
             const body = {
                 name: name,
@@ -42,7 +54,7 @@ export default function Form() {
                 await sendMail(body);
                 history.push("/");
             } catch (e) {
-                console.log(e);
+                console.error(e);
             }
         }
     }
@@ -50,21 +62,24 @@ export default function Form() {
     const { name, email, text } = values;
 
     return (
-        <Container>
-            <form onSubmit={handleSubmit}>
-                <p>Preencha os campos abaixo, que nós enviaremos um e-mail anônimo para o seu crush!</p>
+        <>
+            <Container>
+                <form onSubmit={handleSubmit}>
+                    <p>Preencha os campos abaixo, que nós enviaremos um e-mail anônimo para o seu crush!</p>
 
-                <label>Nome do crush</label>
-                <input onChange={handleChange('name')} placeholder="Nome" type="text" />
+                    <label>Nome do crush</label>
+                    <input onChange={handleChange('name')} placeholder="Nome" type="text" />
 
-                <label>E-mail</label>
-                <input onChange={handleChange('email')} placeholder="Email" type="email" />
+                    <label>E-mail</label>
+                    <input onChange={handleChange('email')} placeholder="Email" type="email" />
 
-                <label>Mensagem</label>
-                <textarea onChange={handleChange('text')} placeholder="Mensagem..." type="text" />
+                    <label>Mensagem</label>
+                    <textarea onChange={handleChange('text')} placeholder="Mensagem..." type="text" />
 
-                <button type="submit">ENVIAR</button>
-            </form>
-        </Container>
+                    <button type="submit">ENVIAR</button>
+                </form>
+            </Container>
+            <ToastContainer />
+        </>
     );
 }
